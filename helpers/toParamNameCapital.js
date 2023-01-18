3404275c3ba2a31cdb0e5741af5ab1366dce4a51
Gokberk Kocak <gokberkkocak@gmail.com>
2023-01-18 14:57:20 +0000

    feat: add api model

---
+++ helpers/toParamNameCapital.js
@@ -0,0 +1,7 @@
+const toParamNameCapital = (name) => {
+    name = name.replace(/[^a-z0-9_]/gi, "");
+    return name.charAt(0).toUpperCase() + name.substr(1);
+  };
+  
+  module.exports = toParamNameCapital;
+  
