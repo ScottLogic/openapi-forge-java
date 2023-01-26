5a087394404035f51e3e6e3276556ff186427ad8
Gokberk Kocak <gokberkkocak@gmail.com>
2023-01-26 10:55:21 +0000

    feat: exception handling

---
+++ helpers/complexReturnType.js
@@ -0,0 +1,19 @@
+const typeConvert = require("./typeConvert");
+
+const complexReturnType = (responseSchema) => { 
+    const responseType = typeConvert(responseSchema);
+    switch (responseType) {
+        case "bool":
+        case "int":
+        case "long":
+        case "float":
+        case "double":
+        case "string":
+            return false;
+        case "DateTime":
+        default:
+            return true;
+    }
+}
+
+module.exports = complexReturnType;
