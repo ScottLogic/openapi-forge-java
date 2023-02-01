19cfaeaf9975601dc34181b43fb91368a3a23c3c
Gokberk Kocak <gkocak@scottlogic.com>
2023-02-01 14:58:50 +0000

    chore: add comments to formatter

--- formatter.js
+++ formatter.js
@@ -1,19 +1,23 @@
 var exec = require('child_process').exec;
 
 module.exports = (folder, logLevel) => {
+    // Use mvn process-sources phase from format goal which comes from external com.spotify.fmt
     exec('./mvnw process-sources', {
         cwd: folder
     },
-    function (error, stdout, stderr) {
-        switch (logLevel) {
-            case 2:
-                console.log('Formatter stdout:\n' + stdout);
-                console.log('Formatter stderr:\n' + stderr);
-                if (error !== null) {
-                    console.log('Formatter exec error:\n' + error);
-                }
-            case 1:
-            case 0:
-        }
-    });
+        function (error, stdout, stderr) {
+            switch (logLevel) {
+                // Verbose
+                case 2:
+                    console.log('Formatter stdout:\n' + stdout);
+                    console.log('Formatter stderr:\n' + stderr);
+                    if (error !== null) {
+                        console.log('Formatter exec error:\n' + error);
+                    }
+                // Standard
+                case 1:
+                // Quiet
+                case 0:
+            }
+        });
 };
