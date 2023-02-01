6977272351b8ddf46e1b574344d98788c433ff48
gkocak-scottlogic <121228726+gkocak-scottlogic@users.noreply.github.com>
2023-02-01 14:49:59 +0000

    Pretty formatter.js
    Co-authored-by: Matthew Smith <masmith@scottlogic.com>

--- formatter.js
+++ formatter.js
@@ -4,16 +4,16 @@ module.exports = (folder, logLevel) => {
     exec('./mvnw process-sources', {
         cwd: folder
     },
-        function (error, stdout, stderr) {
-            switch (logLevel) {
-                case 2:
-                    console.log('Formatter stdout:\n' + stdout);
-                    console.log('Formatter stderr:\n' + stderr);
-                    if (error !== null) {
-                        console.log('Formatter exec error:\n' + error);
-                    }
-                case 1:
-                case 0:
-            }
-        });
+    function (error, stdout, stderr) {
+        switch (logLevel) {
+            case 2:
+                console.log('Formatter stdout:\n' + stdout);
+                console.log('Formatter stderr:\n' + stderr);
+                if (error !== null) {
+                    console.log('Formatter exec error:\n' + error);
+                }
+            case 1:
+            case 0:
+        }
+    });
 };
