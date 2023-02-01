b075d771ed326c67d4e3422e3c695ffe6b836655
Gokberk Kocak <gkocak@scottlogic.com>
2023-02-01 15:01:18 +0000

    chore: pretty indent on formatter

--- formatter.js
+++ formatter.js
@@ -5,19 +5,19 @@ module.exports = (folder, logLevel) => {
     exec('./mvnw process-sources', {
         cwd: folder
     },
-        function (error, stdout, stderr) {
-            switch (logLevel) {
-                // Verbose
-                case 2:
-                    console.log('Formatter stdout:\n' + stdout);
-                    console.log('Formatter stderr:\n' + stderr);
-                    if (error !== null) {
-                        console.log('Formatter exec error:\n' + error);
-                    }
-                // Standard
-                case 1:
-                // Quiet
-                case 0:
-            }
-        });
+    function (error, stdout, stderr) {
+        switch (logLevel) {
+            // Verbose
+            case 2:
+                console.log('Formatter stdout:\n' + stdout);
+                console.log('Formatter stderr:\n' + stderr);
+                if (error !== null) {
+                    console.log('Formatter exec error:\n' + error);
+                }
+            // Standard
+            case 1:
+            // Quiet
+            case 0:
+        }
+    });
 };
