6e95ea9e853219c507dca0d32a8ffdfc66abb9d0
Gokberk Kocak <gkocak@scottlogic.com>
2023-02-02 11:08:22 +0000

    feat: platform agnostic fmt for generated code

--- formatter.js
+++ formatter.js
@@ -1,8 +1,17 @@
 var exec = require('child_process').exec;
+const platform = require("process").platform;
 
 module.exports = (folder, logLevel) => {
-    // Use mvn process-sources phase from format goal which comes from external com.spotify.fmt
-    exec('./mvnw process-sources', {
+    cmd = "";
+    // Use platform information to determine the OS and use platform specific maven wrapper.
+    if (platform == "win32") {
+        cmd = ".\\mvnw.cmd"
+    } else {
+        cmd = './mvnw'
+    }
+    // Use mvn process-sources phase from format goal that comes from external com.spotify.fmt
+    cmd += " process-sources"
+    exec(cmd, {
         cwd: folder
     },
     function (error, stdout, stderr) {
