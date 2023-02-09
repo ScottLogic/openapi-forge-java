0dc37750092e56785b44685227783826c19735dc
Gokberk Kocak <gkocak@scottlogic.com>
2023-02-09 10:36:43 +0000

    Feat: change formatting to not have java dep

--- formatter.js
+++ formatter.js
@@ -1,32 +1,14 @@
-var exec = require('child_process').exec;
-const platform = require("process").platform;
+const cli = require("prettier/cli.js");
 
-module.exports = (folder, logLevel) => {
-    cmd = "";
-    // Use platform information to determine the OS and use platform specific maven wrapper.
-    if (platform == "win32") {
-        cmd = ".\\mvnw.cmd"
-    } else {
-        cmd = './mvnw'
-    }
-    // Use mvn process-sources phase from format goal that comes from external com.spotify.fmt
-    cmd += " process-sources"
-    exec(cmd, {
-        cwd: folder
-    },
-    function (error, stdout, stderr) {
-        switch (logLevel) {
-            // Verbose
-            case 2:
-                console.log('Formatter stdout:\n' + stdout);
-                console.log('Formatter stderr:\n' + stderr);
-                if (error !== null) {
-                    console.log('Formatter exec error:\n' + error);
-                }
-            // Standard
-            case 1:
-            // Quiet
-            case 0:
-        }
-    });
-};
+const logLevels = [
+    /* quiet */
+    "silent",
+    /* standard */
+    "warn",
+    /* verbose */
+    "debug",
+  ];
+  
+  module.exports = (folder, logLevel) => {
+    return cli.run(["--write", folder, "--loglevel", logLevels[logLevel]]);
+  };
