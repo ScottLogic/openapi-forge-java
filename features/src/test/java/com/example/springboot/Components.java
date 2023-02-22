f67fad4b7ee29311e8a7f4b33e91dd582c2cd918
Matthew Smith <masmith@scottlogic.com>
2023-02-22 17:09:00 +0000

    chore: add type step definition check

--- features/src/test/java/com/example/springboot/Components.java
+++ features/src/test/java/com/example/springboot/Components.java
@@ -1,23 +1,28 @@
 package com.example.springboot;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import java.util.ArrayList;
 
 public class Components {
+  private Object latestResponse;
+
   @When("calling the method {word} and the server responds with")
   public void calling_method_server_responds(String method, String response) {
     System.err.println(method);
     System.err.println(response);
-    CommonDefinitions.callMethod(method, new ArrayList<>(), response);
+    latestResponse = CommonDefinitions.callMethod(method, new ArrayList<>(), response);
     //    throw new io.cucumber.java.PendingException();
   }
 
   @Then("the response should be of type {word}")
   public void response_of_type(String type) {
-    System.err.println(type);
-    throw new io.cucumber.java.PendingException();
+    assertEquals(type, latestResponse.getClass().getSimpleName());
+    //    System.err.println(type);
+    //    throw new io.cucumber.java.PendingException();
   }
 
   @And("the response should have a property {word} with value {word}}")
