c8f6731e7c15df77fb8f71d5fac5455feb46a1a5
Gokberk Kocak <gkocak@scottlogic.com>
2023-03-01 11:42:11 +0000

    feat: dynamic package name via post-processing

---
+++ postProcess.js
@@ -0,0 +1,10 @@
+const shell = require("shelljs")
+  
+module.exports = (folder, _, options) => {
+  var package_name = options["generator.package"];
+  var package_folder = package_name.replaceAll(".","/");
+  java_path = "src/main/java/" + package_folder;
+  shell.cd(folder);
+  shell.mkdir("-p", java_path);
+  shell.mv("-f", "*.java", java_path);
+};
