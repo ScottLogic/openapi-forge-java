f1b4c098a0963ca54a54c116c806b499fdd7501b
Gokberk Kocak <gkocak@scottlogic.com>
2023-03-09 16:50:18 +0000

    fix: remove model split logic from branch

--- postProcess.js
+++ postProcess.js
@@ -1,7 +1,5 @@
-const model_fix = require("./postProcessModel");
 const move_files = require("./postProcessMoveFiles");
 
 module.exports = (folder, _, options) => {
-  model_fix(folder);
   move_files(folder, options);
 };
