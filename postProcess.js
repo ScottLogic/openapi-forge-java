aad08dbebfce90f1bcf9f57204da6607f1b27d0b
Gokberk Kocak <gkocak@scottlogic.com>
2023-03-09 10:40:54 +0000

    chore: split move file logic

--- postProcess.js
+++ postProcess.js
@@ -1,12 +1,7 @@
-const shell = require("shelljs");
 const model_fix = require("./postProcessModel");
+const move_files = require("./postProcessMoveFiles");
 
 module.exports = (folder, _, options) => {
   model_fix(folder);
-  var package_name = options["generator.package"];
-  var package_folder = package_name.replaceAll(".", "/");
-  java_path = "src/main/java/" + package_folder;
-  shell.cd(folder);
-  shell.mkdir("-p", java_path);
-  shell.mv("-f", "*.java", java_path);
+  move_files(folder, options);
 };
