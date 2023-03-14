443f6362ed1012eda40fc9d85f98d3d83d6d62a5
Gokberk Kocak <gkocak@scottlogic.com>
2023-03-14 10:27:56 +0000

    chore: add description to post process move

--- postProcessMoveFiles.js
+++ postProcessMoveFiles.js
@@ -1,5 +1,10 @@
 const shell = require("shelljs");
 
+// To match the java package/folder conventions, the java files that are generated from handlebars templates 
+// should be moved to src/main/java/<package_folder>. 
+// The package folder name comes from the generator options and that can given as a command line argument
+// by --generator.package
+
 module.exports = (folder, options) => {
   var package_name = options["generator.package"];
   var package_folder = package_name.replaceAll(".", "/");
