44646c9c7927aa196c03683532cb676266c84865
Matthew Smith <masmith@scottlogic.com>
2023-03-14 09:55:32 +0000

    fix: request HTTP verbs when request has a body

--- helpers/queryTypeWithBody.js
+++ helpers/queryTypeWithBody.js
@@ -1,16 +1,6 @@
 // If the params are given to a helper with a unfolded @key, params parameter gets unfolded as well.
 // This is why this function exists seperately.
 
-const queryTypeWithBody = (key) => {
-    if (key == "delete") {
-        return ".delete()";
-    } else if (key == "put") {
-        return ".put(httpBody)"
-    } else if (key == "post") {
-        return ".post(httpBody)"
-    } else {
-        return "";
-    }
-};
+const queryTypeWithBody = (httpVerb) => `.method("${httpVerb}", httpBody)`;
 
 module.exports = queryTypeWithBody;
