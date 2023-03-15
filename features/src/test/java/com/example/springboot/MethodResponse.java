88aa3f700873b8eea76f17579221ab0b5e82c3db
Matthew Smith <masmith@scottlogic.com>
2023-03-15 16:01:45 +0000

    chore: add helper method to get request body

--- features/src/test/java/com/example/springboot/MethodResponse.java
+++ features/src/test/java/com/example/springboot/MethodResponse.java
@@ -1,7 +1,10 @@
 package com.example.springboot;
 
+import java.io.IOException;
+import java.util.Objects;
 import okhttp3.Headers;
 import okhttp3.Request;
+import okio.Buffer;
 
 public class MethodResponse {
   private final Object resultOfMethodCall;
@@ -30,7 +33,24 @@ public class MethodResponse {
     return request.method();
   }
 
+  public String getRequestBodyAsString() {
+    return bodyToString(request);
+  }
+
   public ClassLoader getClassLoader() {
     return classLoader;
+  }
+
+  // https://stackoverflow.com/a/29033727/
+  private String bodyToString(final Request request) {
+    try {
+      final Request copy = request.newBuilder().build();
+      final Buffer buffer = new Buffer();
+      Objects.requireNonNull(copy.body()).writeTo(buffer);
+      return buffer.readUtf8();
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
   }
 }
