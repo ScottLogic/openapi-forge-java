ab7fd4aedfbd626973a4c85a58e12590bb1ae41d
Matthew Smith <masmith@scottlogic.com>
2023-03-15 15:30:10 +0000

    chore: add support for custom type serialisation in cucumber tests (objects.feature)

--- features/src/test/java/com/example/springboot/TypeConverter.java
+++ features/src/test/java/com/example/springboot/TypeConverter.java
@@ -1,5 +1,7 @@
 package com.example.springboot;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Arrays;
 
 public class TypeConverter {
@@ -24,6 +26,15 @@ public class TypeConverter {
         convertedValues[i] = Float.valueOf(values[i]);
       } else if (type == String.class) {
         convertedValues[i] = values[i];
+      } else if (type.toString().contains(this.getClass().getPackageName())) {
+        // This is a custom type, defined in the schema.
+        // We need to deserialise the String to the Object of the correct class.
+        ObjectMapper deserMapper = new ObjectMapper();
+        try {
+          convertedValues[i] = deserMapper.readValue(values[i], type);
+        } catch (JsonProcessingException e) {
+          throw new RuntimeException(e);
+        }
       } else {
         throw new UnsupportedOperationException(
             "Trying to convert value " + values[i] + " from String to " + type);
