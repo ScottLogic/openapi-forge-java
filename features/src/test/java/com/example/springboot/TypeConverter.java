9a64a17d813f61ee641454a5de1fc3a7a1368332
Matthew Smith <masmith@scottlogic.com>
2023-03-16 14:39:35 +0000

    chore: refactor type converter

--- features/src/test/java/com/example/springboot/TypeConverter.java
+++ features/src/test/java/com/example/springboot/TypeConverter.java
@@ -11,38 +11,41 @@ public class TypeConverter {
     String[] values = fillOutWithNull(valuesWithoutOptionalParameters, targetTypes.length);
     Object[] convertedValues = new Object[values.length];
     for (int i = 0; i < values.length; i++) {
-      Class<?> type = targetTypes[i];
-      if (values[i] == null) {
-        convertedValues[i] = null;
-      } else if (type == Integer.class) {
-        convertedValues[i] = Integer.valueOf(values[i]);
-      } else if (type == Long.class) {
-        convertedValues[i] = Long.valueOf(values[i]);
-      } else if (type == Double.class) {
-        convertedValues[i] = Double.valueOf(values[i]);
-      } else if (type == Float.class) {
-        convertedValues[i] = Float.valueOf(values[i]);
-      } else if (type == String.class) {
-        convertedValues[i] = values[i];
-      } else if (type.toString().contains(this.getClass().getPackageName())) {
-        // This is a custom type, defined in the schema.
-        // We need to deserialise the String to the Object of the correct class.
-        ObjectMapper deserMapper = new ObjectMapper();
-        try {
-          convertedValues[i] = deserMapper.readValue(values[i], type);
-        } catch (JsonProcessingException e) {
-          throw new RuntimeException(e);
-        }
-      } else if (type.toString().contains("java.util.List")) {
-        // Assuming a list of strings.
-        String[] strings = values[i].split(",");
-        convertedValues[i] = Arrays.asList(strings);
-      } else {
-        throw new UnsupportedOperationException(
-            "Trying to convert value " + values[i] + " from String to " + type);
-      }
+      convertedValues[i] = convertValue(values[i], targetTypes[i]);
     }
     return convertedValues;
+  }
+
+  private Object convertValue(String value, Class<?> type) {
+    if (value == null) {
+      return null;
+    } else if (type == Integer.class) {
+      return Integer.valueOf(value);
+    } else if (type == Long.class) {
+      return Long.valueOf(value);
+    } else if (type == Double.class) {
+      return Double.valueOf(value);
+    } else if (type == Float.class) {
+      return Float.valueOf(value);
+    } else if (type == String.class) {
+      return value;
+    } else if (type.toString().contains(this.getClass().getPackageName())) {
+      // This is a custom type, defined in the schema.
+      // We need to deserialise the String to the Object of the correct class.
+      ObjectMapper deserMapper = new ObjectMapper();
+      try {
+        return deserMapper.readValue(value, type);
+      } catch (JsonProcessingException e) {
+        throw new RuntimeException(e);
+      }
+    } else if (type.toString().contains("java.util.List")) {
+      // Assuming a list of strings.
+      String[] strings = value.split(",");
+      return Arrays.asList(strings);
+    } else {
+      throw new UnsupportedOperationException(
+          "Trying to convert value " + value + " from String to " + type);
+    }
   }
 
   private String[] fillOutWithNull(String[] valuesWithoutOptionalParameters, int length) {
