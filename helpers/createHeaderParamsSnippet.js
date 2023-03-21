f74364dca3d7cb0c1a028d08e4168277948dea7f
Gokberk Kocak <gkocak@scottlogic.com>
2023-03-21 14:46:36 +0000

    fix: multi cookie header fix

--- helpers/createHeaderParamsSnippet.js
+++ helpers/createHeaderParamsSnippet.js
@@ -9,13 +9,13 @@ const createHeaderParamsSnippet = (sortedParams) => {
   let cookieParams = getParametersByType(sortedParams, "cookie");
   if (cookieParams.length !== 0) {
     let safeParamName = toParamName(cookieParams[0].name);
-    headerSnippet += `.addHeader("cookie", "${cookieParams[0].name}="+${safeParamName}+"`;
+    headerSnippet += `.addHeader("cookie", "${cookieParams[0].name}="+${safeParamName}`;
     cookieParams = cookieParams.slice(1);
     for (const cookieParam of cookieParams) {
       safeParamName = toParamName(cookieParam.name);
       headerSnippet += ` + ";${cookieParam.name}=" + ${safeParamName}`;
     }
-    headerSnippet += '")\n';
+    headerSnippet += ')\n';
   }
 
   const headerParams = getParametersByType(sortedParams, "header");
