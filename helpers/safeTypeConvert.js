bebe541733864f7f6624479f255b9f77f7c9cf41
Gokberk Kocak <gkocak@scottlogic.com>
2023-04-13 14:13:26 +0100

    chore: apply feedback

--- helpers/safeTypeConvert.js
+++ helpers/safeTypeConvert.js
@@ -1,9 +1,6 @@
 const Handlebars = require("handlebars");
 const typeConvert = require("./typeConvert");
 
-// If safeTypeConvert is called without the optional arguments, these arguments defaults to false
-// For inFnSignature, this variant should be only called in function definitions to provide necessary
-//  necessary annotations for the public api.
 const safeTypeConvert = (prop, shouldBox = false, inFnSignature = false) => {
   if (typeof shouldBox !== "boolean") {
     shouldBox = false;
