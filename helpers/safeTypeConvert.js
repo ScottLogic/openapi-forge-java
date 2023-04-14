c457712c0b0c167fb4385670a4d30f34120368d7
gkocak-scottlogic <121228726+gkocak-scottlogic@users.noreply.github.com>
2023-04-14 15:00:24 +0100

    New Feature: required object annotations for boxed types (#71)
    * feat: required object annotations
    
    * fix: match fn signature for unsupported methods
    
    * fix: required field access in _sortedParams
    
    * fix: fix required for unsupported
    
    * fix: prettier
    
    * chore: rename inDec to inFnSignature
    
    * fix: annotations on method body
    
    * fix: let to const from suggestion on 2 diff places
    
    * chore: apply feedback
    
    * chore: run prettier

--- helpers/safeTypeConvert.js
+++ helpers/safeTypeConvert.js
@@ -1,11 +1,14 @@
 const Handlebars = require("handlebars");
 const typeConvert = require("./typeConvert");
 
-const safeTypeConvert = (prop, shouldBox = false) => {
+const safeTypeConvert = (prop, shouldBox = false, inFnSignature = false) => {
   if (typeof shouldBox !== "boolean") {
     shouldBox = false;
   }
-  return new Handlebars.SafeString(typeConvert(prop, shouldBox));
+  if (typeof inFnSignature !== "boolean") {
+    inFnSignature = false;
+  }
+  return new Handlebars.SafeString(typeConvert(prop, shouldBox, inFnSignature));
 };
 
 module.exports = safeTypeConvert;
--- helpers/typeConvert.js
+++ helpers/typeConvert.js
@@ -1,6 +1,7 @@
 const toSafeName = require("./toClassName");
 
-const fromFormat = (propFormat, shouldBox) => {
+const fromFormat = (propFormat, shouldBox, inFnSignature) => {
+  const notNullPrefix = !shouldBox && inFnSignature ? "@NonNull " : "";
   switch (propFormat) {
     case "int32":
       return shouldBox ? "Integer" : "int";
@@ -11,19 +12,26 @@ const fromFormat = (propFormat, shouldBox) => {
     case "double":
       return shouldBox ? "Double" : "double";
     case "date":
-      return "LocalDate";
+      return notNullPrefix + "LocalDate";
     case "date-time":
-      return "ZonedDateTime";
+      return notNullPrefix + "ZonedDateTime";
     case "byte":
     case "binary":
     case "string":
-      return "String";
+      return notNullPrefix + "String";
     default:
       return shouldBox ? "Void" : "void";
   }
 };
 
-const fromType = (propType, additionalProperties, items, shouldBox) => {
+const fromType = (
+  propType,
+  additionalProperties,
+  items,
+  shouldBox,
+  inFnSignature
+) => {
+  const notNullPrefix = !shouldBox && inFnSignature ? "@NonNull " : "";
   switch (propType) {
     case "integer":
       return shouldBox ? "Integer" : "int";
@@ -32,7 +40,7 @@ const fromType = (propType, additionalProperties, items, shouldBox) => {
     case "boolean":
       return shouldBox ? "Boolean" : "boolean";
     case "string":
-      return "String";
+      return notNullPrefix + "String";
     case "array":
       return `List<${typeConvert(items, true)}>`;
     // inline object definition
@@ -40,26 +48,33 @@ const fromType = (propType, additionalProperties, items, shouldBox) => {
       if (additionalProperties) {
         return `HashMap<String,${typeConvert(additionalProperties, true)}>`;
       } else {
-        return "Object";
+        return notNullPrefix + "Object";
       }
     default:
       return shouldBox ? "Void" : "void";
   }
 };
 
-const typeConvert = (prop, shouldBox = false) => {
+const typeConvert = (prop, shouldBox = false, inFnSignature = false) => {
   if (prop === null) return shouldBox ? "Void" : "void";
 
-  if (prop === undefined) return "object";
+  if (prop === undefined) return "Object";
 
   // resolve references
   if (prop.$ref) {
-    return toSafeName(prop.$ref.split("/").pop());
+    const notNullPrefix = !shouldBox && inFnSignature ? "@NonNull " : "";
+    return notNullPrefix + toSafeName(prop.$ref.split("/").pop());
   }
 
   const type = prop.format
-    ? fromFormat(prop.format, shouldBox)
-    : fromType(prop.type, prop.additionalProperties, prop.items, shouldBox);
+    ? fromFormat(prop.format, shouldBox, inFnSignature)
+    : fromType(
+        prop.type,
+        prop.additionalProperties,
+        prop.items,
+        shouldBox,
+        inFnSignature
+      );
 
   return type === "" ? "object" : type;
 };
