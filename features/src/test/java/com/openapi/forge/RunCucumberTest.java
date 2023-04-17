e39131ba40bb639d0f5b0d9fdc430236bb9524fc
Gokberk Kocak <gkocak@scottlogic.com>
2023-04-17 12:43:51 +0100

    chore: skip one bdd test

--- features/src/test/java/com/openapi/forge/RunCucumberTest.java
+++ features/src/test/java/com/openapi/forge/RunCucumberTest.java
@@ -1,5 +1,6 @@
 package com.openapi.forge;
 
+import static io.cucumber.junit.platform.engine.Constants.FILTER_NAME_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
@@ -13,4 +14,9 @@ import org.junit.platform.suite.api.Suite;
 @SelectClasspathResource("com/openapi/forge")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.openapi.forge")
+// dont run one particular scenario with a get request with body.
+@ConfigurationParameter(
+  key = FILTER_NAME_PROPERTY_NAME,
+  value = "^(?!Calling API methods with a request body).*$"
+)
 public class RunCucumberTest {}
