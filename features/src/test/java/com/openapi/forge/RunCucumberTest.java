923b94ae1cd62b19dc49589b67ced13d0fd19458
gkocak-scottlogic <121228726+gkocak-scottlogic@users.noreply.github.com>
2023-04-21 11:35:22 +0100

    Chore: skip one bdd test (#72)
    * chore: skip one bdd test
    
    * chore: update config comment

--- features/src/test/java/com/openapi/forge/RunCucumberTest.java
+++ features/src/test/java/com/openapi/forge/RunCucumberTest.java
@@ -1,5 +1,6 @@
 package com.openapi.forge;
 
+import static io.cucumber.junit.platform.engine.Constants.FILTER_NAME_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
@@ -13,4 +14,10 @@ import org.junit.platform.suite.api.Suite;
 @SelectClasspathResource("com/openapi/forge")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.openapi.forge")
+// Skip a test scenario where a non-standard behaviour of GET request with requestBody is used.
+// See discussion in https://github.com/ScottLogic/openapi-forge/issues/182.
+@ConfigurationParameter(
+  key = FILTER_NAME_PROPERTY_NAME,
+  value = "^(?!Calling API methods with a request body).*$"
+)
 public class RunCucumberTest {}
